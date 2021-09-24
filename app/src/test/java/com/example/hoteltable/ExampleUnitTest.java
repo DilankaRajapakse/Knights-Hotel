1192bf215b30c22bba5cbc321519b8cd94020c1d
DilankaRajapakse <it20019518@my.sliit.lk>
2021-09-24 10:23:40 +0530

    added files

--- app/src/test/java/com/example/hoteltable/ExampleUnitTest.java
+++ app/src/test/java/com/example/hoteltable/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.hoteltable;
+package com.techMinions.hotelmanagementsystem;
 
 import org.junit.Test;
 
@@ -10,8 +10,70 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void roomtotPriceCalculation() {
+        String input1 = "Single Room";
+        int input2 = 2;
+        int input3 = 3;
+        int output;
+        int expected = 63000;
+        double delta = .1;
+
+        roombooking rm = new roombooking();
+        output = rm.roomtotPriceCalculation(input1, input2, input3);
+
+        assertEquals(expected, output, delta);
+
     }
+
+    /**
+     * IT20019518
+     **/
+    @Test
+    public void tabletotPriceCalculation() {
+        String input1 = "For 2 people";
+        int input2 = 2;
+        int output;
+        int expected = 4000;
+        double delta = .1;
+
+        tables tb = new tables();
+        output = tb.totPriceCalculation(input1, input2);
+
+        assertEquals(expected, output, delta);
+    }
+
+
+    @Test
+    public void halltotPriceCalculation() {
+        String input1 = "Indoor Wedding Hall";
+        int input2 = 2;
+        int output;
+        int expected = 16000;
+        double delta = .1;
+
+        hallsbooking hall = new hallsbooking();
+        output = hall.totPriceCalculation(input1, input2);
+
+        assertEquals(expected, output, delta);
+    }
+
+
+    @Test
+    public void deliverytotPriceCalculation() {
+        String input1 = "Rice And Curry";
+        int input2 = 2;
+        int output;
+        int expected = 500;
+        double delta = .1;
+
+        deliveryorder delivery = new deliveryorder();
+        output = delivery.totPriceCalculation(input1, input2);
+
+        assertEquals(expected, output, delta);
+    }
+
+
 }
